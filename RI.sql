6ef6c60c7b0ca93c5a99284c3babd1d6011578a2
Guilherme Andrade <j.guilherme.andrade@ist.utl.pt>
2015-12-10 17:18:31 +0000

    Update RI.sql
    Já funcionam todas.

--- RI.sql
+++ RI.sql
@@ -8,40 +8,132 @@ e uma vez no universo das relações tipo registo, pagina, campo, registo e valo
 
 Campo idseq no tipo_registo,registo,pagina,campo e valor.
 
-Fazer um Trigger para cada tabela que verifica que o numero posto não existe em nenhuma das tabelas (No Insert e no Update)
-
-VERIFICAR SE NÃO PODEMOS CRIAR UMA STORED PROCEDURE PARA NÃO REPETIR TANTO CÓDIGO
+VERIFICAR SE NÃO PODEMOS CRIAR UMA STORED PROCEDURE PARA NÃO REPETIR TANTO CÓDIGO (se houver tempo)
 
 */
 
-#REALIZAR DOIS TRIGGERS PARA CADA TABELA SEGUINDO ESTES EXEMPLOS:
+# TRIGGERS PAGINA
+
 
 delimiter //
 CREATE TRIGGER pagina_before_insert BEFORE INSERT ON pagina 
 FOR EACH ROW
 BEGIN
-IF EXISTS ( SELECT p.idseq FROM pagina p,tipo_registo tr,registo r,campo c,valor v
-WHERE (NEW.idseq=p.idseq OR NEW.idseq=tr.idseq OR NEW.idseq=r.idseq OR NEW.idseq=c.idseq OR NEW.idseq=v.idseq))
-THEN CALL nao_existo(); 
-END IF;
-END
-//
+INSERT INTO sequencia (moment,userid)
+VALUES (NOW(),NEW.userid);
+SET NEW.idseq= LAST_INSERT_ID(); 
+END; //
 delimiter ;
 
 delimiter //
 CREATE TRIGGER pagina_before_update BEFORE UPDATE ON pagina 
 FOR EACH ROW
 BEGIN
-IF EXISTS ( SELECT p.idseq FROM pagina p,tipo_registo tr,registo r,campo c,valor v
-WHERE (NEW.idseq=p.idseq OR NEW.idseq=tr.idseq OR NEW.idseq=r.idseq OR NEW.idseq=c.idseq OR NEW.idseq=v.idseq))
-THEN CALL nao_existo(); 
-END IF;
-END
-//
+UPDATE sequencia 
+SET moment=NOW() WHERE idseq=OLD.idseq();
+SET idseq= OLD.idseq(); 
+END; //
 delimiter ;
 
+#FIM TRIGGERS PAGINA
 
+# TRIGGERS TIPO_REGISTO
+delimiter //
+CREATE TRIGGER tipo_registo_before_insert BEFORE INSERT ON tipo_registo
+FOR EACH ROW
+BEGIN
+INSERT INTO sequencia (moment,userid)
+VALUES (NOW(),NEW.userid);
+SET NEW.idseq= LAST_INSERT_ID(); 
+END; //
+delimiter ;
+
+delimiter //
+CREATE TRIGGER tipo_registo_before_update BEFORE UPDATE ON tipo_registo 
+FOR EACH ROW
+BEGIN
+UPDATE sequencia 
+SET moment=NOW() WHERE idseq=OLD.idseq();
+SET idseq= OLD.idseq(); 
+END; //
+delimiter ;
+
+#FIM TRIGGERS TIPO_REGISTO
+
+# TRIGGERS REGISTO
+
+delimiter //
+CREATE TRIGGER registo_before_insert BEFORE INSERT ON registo
+FOR EACH ROW
+BEGIN
+INSERT INTO sequencia (moment,userid)
+VALUES (NOW(),NEW.userid);
+SET NEW.idseq= LAST_INSERT_ID(); 
+END; //
+delimiter ;
+
+delimiter //
+CREATE TRIGGER registo_before_update BEFORE UPDATE ON registo 
+FOR EACH ROW
+BEGIN
+UPDATE sequencia 
+SET moment=NOW() WHERE idseq=OLD.idseq();
+SET idseq= OLD.idseq(); 
+END; //
+delimiter ;
+
+# FIM TRIGGERS REGISTO
+
+# TRIGGERS CAMPO
+
+delimiter //
+CREATE TRIGGER campo_before_insert BEFORE INSERT ON campo
+FOR EACH ROW
+BEGIN
+INSERT INTO sequencia (moment,userid)
+VALUES (NOW(),NEW.userid);
+SET NEW.idseq= LAST_INSERT_ID(); 
+END; //
+delimiter ;
+
+delimiter //
+CREATE TRIGGER campo_before_update BEFORE UPDATE ON campo 
+FOR EACH ROW
+BEGIN
+UPDATE sequencia 
+SET moment=NOW() WHERE idseq=OLD.idseq();
+SET idseq= OLD.idseq(); 
+END; //
+delimiter ;
+
+#FIM TRIGGERS CAMPO
+
+# TRIGGERS VALOR
+
+delimiter //
+CREATE TRIGGER valor_before_insert BEFORE INSERT ON valor
+FOR EACH ROW
+BEGIN
+INSERT INTO sequencia (moment,userid)
+VALUES (NOW(),NEW.userid);
+SET NEW.idseq= LAST_INSERT_ID(); 
+END; //
+delimiter ;
+
+delimiter //
+CREATE TRIGGER valor_before_update BEFORE UPDATE ON valor 
+FOR EACH ROW
+BEGIN
+UPDATE sequencia 
+SET moment=NOW() WHERE idseq=OLD.idseq();
+SET idseq= OLD.idseq(); 
+END; //
+delimiter ;
+
+# FIM TRIGGERS VALOR
 
 # TESTES
 
-INSERT INTO pagina (userid, pagecounter, nome, idseq, ativa) VALUES (9999, 9999, "Nike Running Portugal - BVU_PS", 151489, true);
+# PAGINA
+INSERT INTO pagina (userid, pagecounter, nome, idseq, ativa) VALUES (1556, 9999, "Nike Running Portugal - BVU_PS", 151489, true);
+UPDATE pagina SET idseq=151489 WHERE pagecounter=1556 AND userid=1556;
