70fd86fb1a2b1e5f18e9b5af973a6ff774584dd0
TJacob <tomas_martins_9@hotmail.com>
2015-12-11 03:36:58 +0000

    App v0.1.4

--- app/check_field.php
+++ app/check_field.php
@@ -16,17 +16,11 @@
 
 	$connection = new PDO("mysql:host=" . $host. ";dbname=" . $dbname, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 
-	echo("<p>Connected to MySQL database $dbname on $host as user $user</p>\n");
-
 	$sql = "SELECT * FROM campo;";
-
-	echo("<p>Query: " . $sql . "</p>\n");
-
 	$result = $connection->query($sql);
-	
 	$num = $result->rowCount();
 
-	echo("<p>$num records retrieved:</p>\n");
+	echo("<p>$num campos encontrados:</p>\n");
 
 	echo("<table border=\"1\">\n");
 	echo("<tr><td>userid</td><td>typecnt</td><td>campocnt</td><td>idseq</td><td>ativo</td><td>nome</td></tr>\n");
@@ -48,11 +42,7 @@
 	}
 	echo("</table>\n");
 		
-        $connection = null;
-	
-	echo("<p>Connection closed.</p>\n");
-
-	echo("<p>Test completed successfully.</p>\n");
+    $connection = null;
 
 ?>
 <A name="bottom">
